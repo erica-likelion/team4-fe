82a52ab329e8d20c8b62106e5137ea23bfa54d46
javadocq <han18264@naver.com>
2025-08-09 19:59:39 +0900

    feat: 대쉬보드 뼈대 완성

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -3,8 +3,15 @@ import * as S from "../styles/pages/DashBoard.styles";
 export function DashBoard() {
   return (
     <S.DashBoardContainer>
-      <S.DashBoardTitle>대시보드</S.DashBoardTitle>
-      <S.DashBoardContent></S.DashBoardContent>
+      <S.TopWrapper>
+        <S.DashBoardTitle>홍보글 추천</S.DashBoardTitle>
+        <S.Line />
+        <S.DashBoardGrid></S.DashBoardGrid>
+      </S.TopWrapper>
+      <S.BottomWrapper>
+        <S.DashBoardTitle>홍보봇 사용 Tip</S.DashBoardTitle>
+        <S.Line />
+      </S.BottomWrapper>
     </S.DashBoardContainer>
   );
 }
--- src/styles/pages/DashBoard.styles.ts
+++ src/styles/pages/DashBoard.styles.ts
@@ -2,20 +2,45 @@ import styled from "styled-components";
 
 export const DashBoardContainer = styled.div`
   display: flex;
+  height: 100%;
+  width: 90%;
   flex-direction: column;
-  align-items: center;
-  padding: 20px;
+  justify-content: center;
+`;
+
+export const TopWrapper = styled.div`
+  display: flex;
+  height: 607px;
+  flex-direction: column;
+  padding-top: 53px;
 `;
 
 export const DashBoardTitle = styled.h1`
-  font-size: 24px;
-  font-weight: bold;
-  color: #333;
+  margin-left: 43px;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color: #000;
 `;
 
-export const DashBoardContent = styled.div`
+export const Line = styled.div`
+  width: 99%;
+  height: 1px;
+  background-color: #dfdfdf;
+  margin-left: 18px;
+  margin-top: 19px;
+`;
+
+export const DashBoardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  row-gap: 70px;
+  column-gap: 51px;
+`;
+
+export const BottomWrapper = styled.div`
   display: flex;
+  height: 417px;
   flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
 `;
