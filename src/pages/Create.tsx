9f6347dcda640b3edc5c22f33ffa3824cffd6875
javadocq <han18264@naver.com>
2025-08-12 18:56:19 +0900

    feat: add button

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -7,6 +7,7 @@ import HashTag from "../components/HashTag";
 export function Create() {
   const [tagList, setTagList] = useState<string[]>([]);
   const [images, setImages] = useState<{ file: File; preview: string }[]>([]);
+  const [request, setRequest] = useState<string>("");
 
   return (
     <S.CreateContainer>
@@ -14,6 +15,21 @@ export function Create() {
         <StoreInfo />
         <ImageUpload images={images} setImages={setImages} />
         <HashTag tagList={tagList} setTagList={setTagList} />
+        <S.RequestBox>
+          <S.RequestTitle>상세 요청</S.RequestTitle>
+          <S.RequestInput
+            value={request}
+            placeholder={
+              "원하는 문체나 강조할 표현이 있다면 알려주세요.\n예: “부드러운 말투”, “정확한 시간 정보 강조”, “공감가는 문장”, “해시태그 추가”"
+            }
+            onChange={(e) => setRequest(e.target.value)}
+          />
+        </S.RequestBox>
+        <S.CreateButtonWrap>
+          <S.CreateButton>
+            <S.CreateButtonText>제작하기</S.CreateButtonText>
+          </S.CreateButton>
+        </S.CreateButtonWrap>
       </S.LeftContainer>
       {/* right */}
     </S.CreateContainer>
--- src/styles/pages/Create.styles.ts
+++ src/styles/pages/Create.styles.ts
@@ -15,3 +15,63 @@ export const LeftContainer = styled.div`
   width: 50%;
   min-width: 643px;
 `;
+
+export const RequestBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22.5px;
+  margin-top: 42px;
+`;
+
+export const RequestTitle = styled.p`
+  color: #000;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const RequestInput = styled.textarea`
+  width: 554px;
+  height: 120px;
+  padding: 14px 16px;
+  border: 1px solid #acacac;
+  border-radius: 10px;
+  resize: none;
+  color: #000;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  ::placeholder {
+    color: #4f4f4f;
+  }
+`;
+export const CreateButtonWrap = styled.div`
+  width: 554px;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 28px;
+`;
+
+export const CreateButton = styled.button`
+  display: flex;
+  width: 170px;
+  height: 44px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background-color: #ff6700;
+  margin-top: 56px;
+  flex-shrink: 0;
+  border: none;
+  cursor: pointer;
+`;
+
+export const CreateButtonText = styled.p`
+  color: #fff;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
